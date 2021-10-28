fac32e9c581abc7b14de98bd1417ca8fbcbcdeec
ezequiel <ezequielcinalli96@gmail.com>
2021-10-28 09:15:33 -0300

    Improved build script and reuploaded docs/

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyC8aac-M9ST811YShr3pK3sjtG98Oeof_4',
+    authDomain: 'angular-todotasklist.firebaseapp.com',
+    databaseURL: 'https://angular-todotasklist.firebaseio.com',
+    projectId: 'angular-todotasklist',
+    storageBucket: 'angular-todotasklist.appspot.com',
+    messagingSenderId: '981774828572',
+    appId: '1:981774828572:web:e917beed1e3b7ec18f0bd9',
+  },
 };
